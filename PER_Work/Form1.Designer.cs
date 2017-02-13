931f96a5b031a5b900de1faffed87da5fdb73807
Ninjax Gamer <Ninjax_gamer@hotmail.com>
2017-02-13 17:17:02 -0200

    Adição de outra form

--- PER_Work/Form1.Designer.cs
+++ PER_Work/Form1.Designer.cs
@@ -30,23 +30,92 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(intro));
             this.start = new System.Windows.Forms.Button();
+            this.dificuldade = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // start
             // 
-            this.start.Location = new System.Drawing.Point(106, 199);
+            this.start.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.start.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.start.Location = new System.Drawing.Point(247, 199);
             this.start.Name = "start";
-            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.Size = new System.Drawing.Size(121, 43);
             this.start.TabIndex = 0;
             this.start.Text = "Começar";
             this.start.UseVisualStyleBackColor = true;
             this.start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dificuldade
+            // 
+            this.dificuldade.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dificuldade.FormattingEnabled = true;
+            this.dificuldade.Items.AddRange(new object[] {
+            "Fácil",
+            "Médio",
+            "Hard",
+            "Medieval"});
+            this.dificuldade.Location = new System.Drawing.Point(247, 67);
+            this.dificuldade.Name = "dificuldade";
+            this.dificuldade.Size = new System.Drawing.Size(121, 26);
+            this.dificuldade.TabIndex = 1;
+            this.dificuldade.Text = "Dificuldade";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bem Vindo ao Jogo";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Perguntas e Repostas";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(244, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 16);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Escolha a:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Image = global::PER_Work.Properties.Resources.Quiz_Games_red;
+            this.pictureBox1.Location = new System.Drawing.Point(31, 120);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(161, 161);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // intro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(385, 303);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dificuldade);
             this.Controls.Add(this.start);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
@@ -54,13 +123,20 @@
             this.Name = "intro";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Introdução";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button start;
+        private System.Windows.Forms.ComboBox dificuldade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- PER_Work/Form1.cs
+++ PER_Work/Form1.cs
@@ -19,7 +19,15 @@ namespace PER_Work
 
         private void button1_Click(object sender, EventArgs e)
         {
+            string difi = Convert.ToString(dificuldade.Text);
+            if (difi == "Médio")
+            MessageBox.Show(difi);
 
+            //Info I1 = new Info(id_jogador);
+            //Pergunta1 P2 = new Pergunta1(id_jogador); Pergunta2 P2 = new Pergunta2(id_jogador);
+            //Pergunta3 P3 = new Pergunta3(id_jogador); Pergunta4 P4 = new Pergunta4(id_jogador);
+
+           //I1.ShowDialog(); P1.ShowDialog(); P2.ShowDialog(); P3.ShowDialog(); P4.ShowDialog();
         }
     }
 }
